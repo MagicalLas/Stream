bb1d512b0e8f412dcb453875871c21560674327f
Las-Wonho <haho013039@gmail.com>
2018-08-05 08:44:35 +0900

    init project
    Add Project
    - Main Stream Project
    - unit Test Project

---
+++ Stream/Stream.cpp
@@ -0,0 +1,12 @@
+﻿// Stream.cpp : 응용 프로그램의 진입점을 정의합니다.
+//
+
+#include "Stream.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ UnitTest/unittest1.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: 테스트 코드를 여기에 입력합니다.
+		}
+
+	};
+}
